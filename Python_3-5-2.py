462e2ff80ab45845c1da8bbe36f9e1d2292650ae
Zawaro <ari.oravasaari@gmail.com>
2016-12-18 14:03:08 +0200

    Create Python_3-5-2.py

---
+++ Python_3-5-2.py
@@ -0,0 +1,13 @@
+#Fizz Buzz test in Python 3.5.2
+
+i = 1
+while (i < 101):
+  if (i % 3 == 0 and i % 5 == 0):
+    print ("FizzBuzz")
+  elif (i % 3 == 0):
+    print ("Fizz")
+  elif (i % 5 == 0):
+    print ("Buzz")
+  else:
+    print (i)
+  i = i + 1
